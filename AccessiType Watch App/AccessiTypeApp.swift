be90f7db8ee4fc279dc089b27148bfbd169b178e
Atri Vyas <atrivyas7@gmail.com>
2024-02-10 15:42:35 -0500

    Initial Commit

---
+++ AccessiType Watch App/AccessiTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessiTypeApp.swift
+//  AccessiType Watch App
+//
+//  Created by Atri Vyas on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessiType_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessiType/AccessiTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessiTypeApp.swift
+//  AccessiType
+//
+//  Created by Atri Vyas on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessiTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
